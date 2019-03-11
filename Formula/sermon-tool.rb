d0b4e85de11a414601fddde344af64ada547e884
David Bellingroth <spam.dbellingroth@gmail.com>
2019-03-11 22:39:08 +0100

    Added formula for sermon-tool

---
+++ Formula/sermon-tool.rb
@@ -0,0 +1,19 @@
+class SermonTool < Formula
+  desc "A tool for converting and upload sermon files"
+  homepage ""
+  head "https://github.com/dbellingroth/sermon-tool.git"
+
+  depends_on "dialog"
+  depends_on "jq"
+  depends_on "atomicparsley"
+  depends_on "ffmpeg" => "with-fdk-aacjq"
+
+  def install
+    system "mkdir -p #{prefix}/bin"
+    system "cp sermon #{prefix}/bin/"
+  end
+
+  test do
+    system "which sermon"
+  end
+end
